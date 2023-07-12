1449c34107f89f98e29162f2b035e723c880c519
Chirag Bhatti <chirag.bhatti@softwire.com>
2023-07-12 15:09:46 +0100

    CLDC-2503: comment out unrequired tf backends

--- terraform/meta/main.tf
+++ terraform/meta/main.tf
@@ -63,77 +63,77 @@ module "terraform_state_backend_non_production" {
 
 # Fails 12 checkovs - All of them LOW
 # Production
-module "terraform_state_backend_production" {
-  source = "cloudposse/tfstate-backend/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  version    = "1.1.1"
-  namespace  = "core"
-  stage      = "production"
-  name       = "terraform"
-  attributes = ["state"]
+#module "terraform_state_backend_production" {
+#  source = "cloudposse/tfstate-backend/aws"
+#  # Cloud Posse recommends pinning every module to a specific version
+#  version    = "1.1.1"
+#  namespace  = "core"
+#  stage      = "production"
+#  name       = "terraform"
+#  attributes = ["state"]
+#
+#block_public_acls                  = true
+#block_public_policy                = true
+#bucket_enabled                     = true
+#bucket_ownership_enforced_enabled  = true
+#dynamodb_enabled                   = true
+#dynamodb_table_name                = "core-production-terraform-state-lock"
+#enable_point_in_time_recovery      = true
+#enable_public_access_block         = true
+#enabled                            = true
+#environment                        = "meta"
+#force_destroy                      = true
+#ignore_public_acls                 = true
+#prevent_unencrypted_uploads        = true
+#restrict_public_buckets            = true
+#s3_bucket_name                     = "core-production-terraform-state"
+#terraform_backend_config_file_path = "."
+#terraform_backend_config_file_name = "backend_production.tf"
+#}
 
-block_public_acls                  = true
-block_public_policy                = true
-bucket_enabled                     = true
-bucket_ownership_enforced_enabled  = true
-dynamodb_enabled                   = true
-dynamodb_table_name                = "core-production-terraform-state-lock"
-enable_point_in_time_recovery      = true
-enable_public_access_block         = true
-enabled                            = true
-environment                        = "meta"
-force_destroy                      = true
-ignore_public_acls                 = true
-prevent_unencrypted_uploads        = true
-restrict_public_buckets            = true
-s3_bucket_name                     = "core-production-terraform-state"
-terraform_backend_config_file_path = "."
-terraform_backend_config_file_name = "backend_production.tf"
-}
 
-
-### NOZAQ ###
-# Fails 13 checkovs - All of them LOW
-provider "aws" {
-  alias = "replica"
-  region = "eu-west-1"
-
-  assume_role {
-    role_arn = "arn:aws:iam::META-ACCOUNT-ID:role/ROLE-NAME"
-  }
-}
-
-module "terraform_state_backend" {
-  source = "nozaq/remote-state-s3-backend/aws"
-
-  providers = {
-    aws         = aws
-    aws.replica = aws.replica
-  }
-
-  dynamodb_enable_server_side_encryption = true
-  dynamodb_table_name = "core-non-production-terraform-states-lock"
-  enable_replication = true
-  iam_role_arn = "arn:aws:iam::815624722760:role/developer"
-  kms_key_alias = "state-key"
-  kms_key_deletion_window_in_days = 30
-  kms_key_enable_key_rotation = true
-  noncurrent_version_expiration = {
-    days = 30
-  }
-  replica_bucket_prefix = "replica"
-  s3_bucket_force_destroy = false
-  s3_bucket_name = "core-non-production-terraform-states"
-  s3_logging_target_bucket = "logging-bucket"
-}
-
-### SQUAREOPS ###
-# Fails 35 checkovs - the below is all we can configure on the backend module
-module "terraform_state_backend" {
-  source = "squareops/tfstate/aws"
-  logging = true
-  environment = "meta"
-  bucket_name = "core-non-production-terraform-states"
-  force_destroy = false
-  versioning_enabled = true
-}
+#### NOZAQ ###
+## Fails 13 checkovs - All of them LOW
+#provider "aws" {
+#  alias = "replica"
+#  region = "eu-west-1"
+#
+#  assume_role {
+#    role_arn = "arn:aws:iam::META-ACCOUNT-ID:role/ROLE-NAME"
+#  }
+#}
+#
+#module "terraform_state_backend" {
+#  source = "nozaq/remote-state-s3-backend/aws"
+#
+#  providers = {
+#    aws         = aws
+#    aws.replica = aws.replica
+#  }
+#
+#  dynamodb_enable_server_side_encryption = true
+#  dynamodb_table_name = "core-non-production-terraform-states-lock"
+#  enable_replication = true
+#  iam_role_arn = "arn:aws:iam::815624722760:role/developer"
+#  kms_key_alias = "state-key"
+#  kms_key_deletion_window_in_days = 30
+#  kms_key_enable_key_rotation = true
+#  noncurrent_version_expiration = {
+#    days = 30
+#  }
+#  replica_bucket_prefix = "replica"
+#  s3_bucket_force_destroy = false
+#  s3_bucket_name = "core-non-production-terraform-states"
+#  s3_logging_target_bucket = "logging-bucket"
+#}
+#
+#### SQUAREOPS ###
+## Fails 35 checkovs - the below is all we can configure on the backend module
+#module "terraform_state_backend" {
+#  source = "squareops/tfstate/aws"
+#  logging = true
+#  environment = "meta"
+#  bucket_name = "core-non-production-terraform-states"
+#  force_destroy = false
+#  versioning_enabled = true
+#}
