20fa160ef3188118b2c157aff533e9d48a7f7c02
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-08-10 12:48:15 +0100

    CLDC-2524: refactor subnet naming and availability zone referencing

--- terraform/modules/networking/subnet.tf
+++ terraform/modules/networking/subnet.tf
@@ -1,33 +1,31 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
 locals {
+  availability_zones  = ["a", "b", "c"]
   public_subnet_cidr  = cidrsubnet(var.vpc_cidr_block, 1, 0)
   private_subnet_cidr = cidrsubnet(var.vpc_cidr_block, 1, 1)
+  region              = "eu-west-2"
 }
 
 resource "aws_subnet" "public" {
-  count             = 3
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count             = length(local.availability_zones)
+  availability_zone = "${local.region}${local.availability_zones[count.index]}"
   # Splits the public CIDR block into three parts (one for each AZ)
   cidr_block = cidrsubnet(local.public_subnet_cidr, 2, count.index)
   vpc_id     = aws_vpc.main.id
 
   tags = {
-    Name = "${var.prefix}-public-subnet-${data.aws_availability_zones.available.names[count.index]}"
+    Name = "${var.prefix}-public-subnet-${local.region}${local.availability_zones[count.index]}"
   }
 }
 
 resource "aws_subnet" "private" {
-  count             = 3
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count             = length(local.availability_zones)
+  availability_zone = "${local.region}${local.availability_zones[count.index]}"
   # Splits the private CIDR block into three parts (one for each AZ)
   cidr_block = cidrsubnet(local.private_subnet_cidr, 2, count.index)
   vpc_id     = aws_vpc.main.id
 
   tags = {
-    Name = "${var.prefix}-private-subnet-${data.aws_availability_zones.available.names[count.index]}"
+    Name = "${var.prefix}-private-subnet-${local.region}${local.availability_zones[count.index]}"
   }
 }
 
