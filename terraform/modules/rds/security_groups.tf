afcb9c9efb6709744f5b46b0272b8aa8e7ee0912
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-08-14 12:27:58 +0100

    CLDC-2527: terraform format

--- terraform/modules/rds/security_groups.tf
+++ terraform/modules/rds/security_groups.tf
@@ -1,17 +1,17 @@
 resource "aws_security_group" "rds" {
- name        = "${var.prefix}-rds-security-group"
- description = "RDS security group"
- vpc_id      = var.vpc_id
+  name        = "${var.prefix}-rds-security-group"
+  description = "RDS security group"
+  vpc_id      = var.vpc_id
 
- ingress {
-   description     = "Allow ingress on port 5432 only from the specified security groups"
-   security_groups = var.security_group_ids
-   from_port       = 5432
-   to_port         = 5432
-   protocol        = "tcp"
- }
+  ingress {
+    description     = "Allow ingress on port 5432 only from the specified security groups"
+    security_groups = var.security_group_ids
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+  }
 
- lifecycle {
-   create_before_destroy = true
- }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
