4a7f7d830b3d7f4a90333dfcd3f3ccc0e0d038f9
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-08-21 15:22:02 +0100

    CLDC-2526: remove unrequired variables files

--- terraform/production/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "parameters" {
-  description = "A map of parameter values. Keys should be a subset of the ones passed to 'parameters' module."
-  type        = map(string)
-  sensitive   = true
-}
