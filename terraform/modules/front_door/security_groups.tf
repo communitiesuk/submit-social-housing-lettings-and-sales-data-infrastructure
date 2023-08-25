3c07138aeec2c7697e6c156e4dd2eb69faed364d
Rachael Booth <rachael.booth@softwire.com>
2023-08-25 17:49:46 +0100

    CLDC-2525: Remove load balancer extra egress rules

--- terraform/modules/front_door/security_groups.tf
+++ terraform/modules/front_door/security_groups.tf
@@ -27,24 +27,6 @@ resource "aws_vpc_security_group_ingress_rule" "load_balancer_https_ingress" {
   security_group_id = aws_security_group.load_balancer.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "load_balancer_http_egress" {
-  description       = "Allow egress to port 80 from all IP addresses"
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  from_port         = 80
-  to_port           = 80
-  security_group_id = aws_security_group.load_balancer.id
-}
-
-resource "aws_vpc_security_group_egress_rule" "load_balancer_https_egress" {
-  description       = "Allow egress to port 443 from all IP addresses"
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  from_port         = 443
-  to_port           = 443
-  security_group_id = aws_security_group.load_balancer.id
-}
-
 resource "aws_vpc_security_group_egress_rule" "load_balancer_container_egress" {
   description       = "Allow egress to port ${var.application_port} from all IP addresses"
   cidr_ipv4         = "0.0.0.0/0"
