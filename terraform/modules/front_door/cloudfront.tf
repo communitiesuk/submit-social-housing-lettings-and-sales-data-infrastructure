e9f666e1a627857d8bfb3ff6bbe1d38bcb1e311a
Sam Seed <sam.seed@softwire.com>
2023-09-01 15:51:02 +0100

    CLDC-2545: use http only for the origin protocol policy

--- terraform/modules/front_door/cloudfront.tf
+++ terraform/modules/front_door/cloudfront.tf
@@ -27,7 +27,7 @@ resource "aws_cloudfront_distribution" "this" {
     custom_origin_config {
       http_port                = 80
       https_port               = 443
-      origin_protocol_policy   = "match-viewer"
+      origin_protocol_policy   = "http-only"
       origin_ssl_protocols     = ["TLSv1.2"]
     }
   }
