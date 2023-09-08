dc8d8caef1259b4146b3f97419a6acb178087156
Sam Seed <sam.seed@softwire.com>
2023-09-08 10:32:31 +0100

    CLDC-2654: output load balancer certificate validation info

--- terraform/development/outputs.tf
+++ terraform/development/outputs.tf
@@ -2,3 +2,8 @@ output "cloudfront_certificate_validation" {
   value       = module.certificates.cloudfront_certificate_validation
   description = "The domain validation objects for the cloudfront certificate"
 }
+
+output "load_balancer_certificate_validation" {
+  value       = module.certificates.load_balancer_certificate_validation
+  description = "The domain validation objects for the load balancer certificate"
+}
