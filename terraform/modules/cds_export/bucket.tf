7e44502bf9d60e2062f7c911df07812522a3deb3
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-09-13 17:07:46 +0100

    CLDC-2764: refactor export bucket iam roles and policies

--- terraform/modules/cds_export/bucket.tf
+++ terraform/modules/cds_export/bucket.tf
@@ -39,24 +39,3 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
     status = "Enabled"
   }
 }
-
-#tfsec:ignore:aws-iam-no-policy-wildcards: require access to all objects in bucket
-data "aws_iam_policy_document" "read_write" {
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.this.arn]
-    effect    = "Allow"
-  }
-
-  statement {
-    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
-    resources = ["${aws_s3_bucket.this.arn}/*"]
-    effect    = "Allow"
-  }
-}
-
-resource "aws_iam_policy" "read_write" {
-  name        = "${var.prefix}-export-bucket-read-write"
-  description = "Policy that allows read/write access to the export bucket"
-  policy      = data.aws_iam_policy_document.read_write.json
-}
--- terraform/modules/cds_export/role_cds.tf
+++ terraform/modules/cds_export/role_cds.tf
@@ -1,6 +1,44 @@
+data "aws_iam_policy_document" "cds_assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.cds_access_role_arn]
+    }
+  }
+}
+
 resource "aws_iam_role" "cds" {
   name               = "${var.prefix}-cds"
   assume_role_policy = data.aws_iam_policy_document.cds_assume_role.json
+}
+
+#tfsec:ignore:aws-iam-no-policy-wildcards:wildcard required to allow access to all files at the root of the bucket
+data "aws_iam_policy_document" "export_bucket_read_only_access" {
+  statement {
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:Describe*"
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "export_bucket_read_only_access" {
+  name   = "${var.prefix}-export-bucket-read-only-access"
+  policy = data.aws_iam_policy_document.export_bucket_read_only_access.json
 }
 
 resource "aws_iam_role_policy_attachment" "export_bucket_read_only_access" {
