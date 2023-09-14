dd40da82a78264637e25d54b617119e0782285eb
Sam Seed <sam.seed@softwire.com>
2023-09-14 16:37:00 +0100

    CLDC-2780: create alarm for app service CPU utilisation

--- terraform/modules/monitoring/variables.tf
+++ terraform/modules/monitoring/variables.tf
@@ -1,3 +1,13 @@
+variable "app_service_name" {
+  type        = string
+  description = "The name of the app service"
+}
+
+variable "ecs_cluster_name" {
+  type        = string
+  description = "The name of the ecs cluster"
+}
+
 variable "prefix" {
   type        = string
   description = "The prefix to be prepended to resource names."
