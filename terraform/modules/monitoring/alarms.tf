311f0c01cdc8820b4d386566c736c32201287230
Sam Seed <sam.seed@softwire.com>
2023-09-18 11:17:18 +0100

    CLDC-2782: add app memory utilisation alarm

--- terraform/modules/monitoring/alarms.tf
+++ terraform/modules/monitoring/alarms.tf
@@ -17,3 +17,23 @@ resource "aws_cloudwatch_metric_alarm" "app_cpu" {
     ServiceName = var.app_service_name
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "app_memory" {
+  alarm_actions             = [aws_sns_topic.this.arn]
+  alarm_name                = "${var.prefix}-app-memory"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  datapoints_to_alarm       = 3
+  evaluation_periods        = 5
+  metric_name               = "MemoryUtilization"
+  namespace                 = "AWS/ECS"
+  ok_actions                = [aws_sns_topic.this.arn]
+  period                    = 60
+  statistic                 = "Average"
+  threshold                 = 80
+  insufficient_data_actions = []
+
+  dimensions = {
+    ClusterName = var.ecs_cluster_name
+    ServiceName = var.app_service_name
+  }
+}
