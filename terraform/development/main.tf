b03ec4c077b9d4a585d1fde66653f2110c26ab9b
Sam Seed <sam.seed@softwire.com>
2023-09-19 14:17:26 +0100

    CLDC-2680: Cloudfront certificate + custom URL

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -122,6 +122,8 @@ module "front_door" {
 
   prefix                = local.prefix
   application_port      = local.application_port
+  cloudfront_certificate_arn = module.certificates.cloudfront_certificate_arn
+  cloudfront_domain_name = local.app_host
   ecs_security_group_id = module.application.ecs_security_group_id
   public_subnet_ids     = module.networking.public_subnet_ids
   vpc_id                = module.networking.vpc_id
