ce1b585eed571709c05113d4d7c599625086f278
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-09-26 14:41:59 +0100

    CLDC-2618: update log group names for ecs clusters

--- terraform/modules/rds_migration/logging.tf
+++ terraform/modules/rds_migration/logging.tf
@@ -3,7 +3,7 @@
 resource "aws_cloudwatch_log_group" "this" {
   #checkov:skip=CKV_AWS_158:TODO CLDC-2779 create and encrypt with a KMS key if retaining this log group
   #checkov:skip=CKV_AWS_338:TODO CLDC-2779 retaining logs for at least 1year greater than necessary for debugging ecs
-  name              = "${var.prefix}-db-migration-ecs-debugging-log"
+  name              = "${var.prefix}-db-migration-ecs"
   retention_in_days = 14
 
   tags = {
