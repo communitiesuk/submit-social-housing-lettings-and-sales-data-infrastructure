f955aa896f9c60e8501a19ca5460876900b9a078
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-09-27 15:58:57 +0100

    CLDC-2546: remove unrequired todos, update waf rules and implement waf protection on load balancer

--- terraform/modules/front_door/load_balancer.tf
+++ terraform/modules/front_door/load_balancer.tf
@@ -1,7 +1,11 @@
+resource "aws_wafv2_web_acl_association" "this" {
+  resource_arn = aws_lb.this.arn
+  web_acl_arn  = aws_wafv2_web_acl.load_balancer.arn
+}
+
 #tfsec:ignore:aws-elb-alb-not-public:the load balancer must be exposed to the internet in order to communicate with cloudfront
 resource "aws_lb" "this" {
   #checkov:skip=CKV_AWS_91:setup access logs on load balancer TODO CLDC-2705
-  #checkov:skip=CKV2_AWS_28:WAF protection to be setup TODO CLDC-2546
   name                       = var.prefix
   drop_invalid_header_fields = true
   enable_deletion_protection = true
---
+++ terraform/modules/front_door/waf_cloudfront.tf
@@ -0,0 +1,276 @@
+# When updating this cloudfront WAF acl, consider making the same change for the load balancer WAF acl, as they generally need to be the same
+resource "aws_wafv2_web_acl" "cloudfront" {
+  #checkov:skip=CKV2_AWS_31:TODO CLDC-2781 setup WAF logging in cloudwatch
+  name        = "${var.prefix}-cloudfront"
+  description = "Web ACL to restrict traffic to CloudFront"
+  provider    = aws.us-east-1
+  scope       = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "waf"
+    sampled_requests_enabled   = false
+  }
+
+  rule {
+    name     = "aws-managed-rules-amazon-ip-reputation-list"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-block-disreputable-ip"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "aws-managed-rules-amazon-anonymous-ip-list"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAnonymousIpList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-block-anonymous-ip"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "aws-managed-rules-common-rule-set"
+    priority = 3
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+        rule_action_override {
+          # This rule blocks request bodies over 8KB in size, but CORE needs file uploads so we remove this restriction
+          # The default maximum request body size that can be inspected when using cloudfront web ACLs is 16KB, so this
+          # does limit the effectiveness of the other rules here. The limit can be increased to up to 64KB if necessary
+          # at extra cost.
+          # More info here: https://docs.aws.amazon.com/waf/latest/developerguide/web-acl-setting-body-inspection-limit.html
+          name = "SizeRestrictions_BODY"
+          action_to_use {
+            allow {}
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-block-common-exploit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "aws-managed-rules-known-bad-inputs-rule-set"
+    priority = 4
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-block-bad-input-exploit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "aws-managed-rules-sqli-rule-set"
+    priority = 5
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-block-sql-exploit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "aws-managed-rules-linux-rule-set"
+    priority = 6
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesLinuxRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-block-linux-exploit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "aws-managed-rules-unix-rule-set"
+    priority = 7
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesUnixRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-block-unix-exploit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "login-ip-rate-limit"
+    priority = 8
+
+    action {
+      block {
+        custom_response {
+          response_code = 429
+        }
+      }
+    }
+
+    statement {
+      rate_based_statement {
+        aggregate_key_type = "IP"
+        limit              = 100
+
+        scope_down_statement {
+          regex_pattern_set_reference_statement {
+            arn = aws_wafv2_regex_pattern_set.waf_rate_limit_urls_cloudwatch.arn
+
+            field_to_match {
+              uri_path {}
+            }
+
+            text_transformation {
+              priority = 0
+              type     = "URL_DECODE"
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-login-ip-rate-limit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "overall-ip-rate-limit"
+    priority = 9
+
+    action {
+      block {
+        custom_response {
+          response_code = 429
+        }
+      }
+    }
+
+    statement {
+      rate_based_statement {
+        aggregate_key_type = "IP"
+        limit              = 2000
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-overall-ip-rate-limit"
+      sampled_requests_enabled   = true
+    }
+  }
+}
+
+resource "aws_wafv2_regex_pattern_set" "waf_rate_limit_urls_cloudwatch" {
+  name     = "${var.prefix}-waf-cloudfront-login-url-regex-patterns"
+  provider = aws.us-east-1
+  scope    = "CLOUDFRONT"
+
+  regular_expression {
+    regex_string = "/account/password/new"
+  }
+
+  regular_expression {
+    regex_string = "/account/password/reset-confirmation"
+  }
+
+  regular_expression {
+    regex_string = "/account/sign-in"
+  }
+
+  regular_expression {
+    regex_string = "/account/two-factor-authentication"
+  }
+
+  regular_expression {
+    regex_string = "/account/two-factor-authentication/resend"
+  }
+}
