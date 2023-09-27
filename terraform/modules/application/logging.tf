b6150b31004e2a1a38bea5a81b5e469007f7b0ba
Sam Seed <sam.seed@softwire.com>
2023-09-27 12:38:30 +0100

    CLDC-2779: set app & vpc flow logs to have 60 day retention

--- terraform/modules/application/logging.tf
+++ terraform/modules/application/logging.tf
@@ -4,7 +4,7 @@ resource "aws_cloudwatch_log_group" "this" {
   #checkov:skip=CKV_AWS_158:TODO CLDC-2542 create and encrypt with a KMS key if retaining this log group
   #checkov:skip=CKV_AWS_338:TODO CLDC-2542 retaining logs for at least 1year greater than necessary for debugging ecs
   name              = "${var.prefix}-ecs-debugging-log"
-  retention_in_days = 14
+  retention_in_days = 60
 
   tags = {
     Application = var.prefix
