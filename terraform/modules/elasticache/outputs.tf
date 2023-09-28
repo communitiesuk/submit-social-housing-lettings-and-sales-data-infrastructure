36246c43ae075047f95b06ba53e6f6b4876763f8
Sam Seed <sam.seed@softwire.com>
2023-09-28 10:07:09 +0100

    CLDC-2756: use reader endpoint, not primary endpoint

--- terraform/modules/elasticache/outputs.tf
+++ terraform/modules/elasticache/outputs.tf
@@ -1,5 +1,5 @@
 output "redis_connection_string" {
-  value       = "redis://${aws_elasticache_replication_group.this.primary_endpoint_address}"
+  value       = "redis://${aws_elasticache_replication_group.this.reader_endpoint_address}"
   description = "A connection string for connecting to the primary redis node, intended to be set in the environment variable REDIS_CONFIG"
   sensitive   = true
 }
