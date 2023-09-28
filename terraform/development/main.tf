5c6e96ad0e142d8273884b752babbe9093a31cad
Sam Seed <sam.seed@softwire.com>
2023-09-28 17:16:09 +0100

    CLDC-2785: create healthy hosts count alarm

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -38,6 +38,7 @@ provider "aws" {
 locals {
   prefix                    = "core-dev"
   app_host                  = ""
+  app_task_desired_count    = 2
   application_port          = 8080
   database_port             = 5432
   load_balancer_domain_name = ""
@@ -51,7 +52,7 @@ module "application" {
   prefix                               = local.prefix
   app_host                             = ""
   app_task_cpu                         = 512
-  app_task_desired_count               = 2
+  app_task_desired_count               = local.app_task_desired_count
   app_task_memory                      = 1024
   application_port                     = local.application_port
   bulk_upload_bucket_access_policy_arn = module.bulk_upload.read_write_policy_arn
@@ -123,6 +124,7 @@ module "front_door" {
   }
 
   prefix                        = local.prefix
+  app_task_desired_count        = local.app_task_desired_count
   application_port              = local.application_port
   cloudfront_certificate_arn    = module.certificates.cloudfront_certificate_arn
   cloudfront_domain_name        = local.app_host
