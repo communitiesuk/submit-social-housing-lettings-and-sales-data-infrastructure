1c3b43ceed2cf88b1aaa720ee162384b65d9ddce
Sam Seed <sam.seed@softwire.com>
2023-10-03 15:45:02 +0100

    CLDC-2548: add automatic response for layer 7 attacks

--- terraform/modules/front_door/shield.tf
+++ terraform/modules/front_door/shield.tf
@@ -4,9 +4,21 @@ resource "aws_shield_protection" "cloudfront" {
   resource_arn = aws_cloudfront_distribution.this.arn
 }
 
+resource "aws_shield_application_layer_automatic_response" "cloudfront" {
+  count        = var.enable_aws_shield ? 1 : 0
+  action       = "BLOCK"
+  resource_arn = aws_cloudfront_distribution.this.arn
+}
+
 resource "aws_shield_protection" "load_balancer" {
   count        = var.enable_aws_shield ? 1 : 0
   name         = "${var.prefix}-load-balancer"
+  resource_arn = aws_lb.this.arn
+}
+
+resource "aws_shield_application_layer_automatic_response" "load_balancer" {
+  count        = var.enable_aws_shield ? 1 : 0
+  action       = "BLOCK"
   resource_arn = aws_lb.this.arn
 }
 
