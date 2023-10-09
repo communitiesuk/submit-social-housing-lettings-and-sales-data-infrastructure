8ab3e5040a804ef27f5e09888756ff29d24beacd
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-10-09 19:59:19 +0100

    CLDC-2534: remove unrequired variable for ecr module

--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -3,11 +3,6 @@ variable "allow_access_by_roles" {
   description = "arns for the roles requiring access to the repository"
 }
 
-variable "prefix" {
-  type        = string
-  description = "The prefix to be prepended to resource names."
-}
-
 variable "sns_topic_arn" {
   type        = string
   description = "The arn of the sns topic"
