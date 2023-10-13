dcec2aff88cfe90459e6c006d1e1be0b0b506e84
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-10-13 14:58:31 +0100

    CLDC-2909: correct the policy version syntax

--- terraform/modules/bulk_upload/bucket.tf
+++ terraform/modules/bulk_upload/bucket.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_policy" "force_ssl" {
   bucket = aws_s3_bucket.this.id
 
   policy = jsonencode({
-    "Version" : "2012-10-17",
+    Version = "2012-10-17",
     Statement = [
       {
         Sid       = "AllowSSLRequestsOnly",
