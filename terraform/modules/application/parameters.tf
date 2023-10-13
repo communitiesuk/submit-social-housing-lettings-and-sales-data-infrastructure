ab3511b6a6997fa263734985853ffa448fb856f6
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-10-13 11:40:55 +0100

    CLDC-2811: fix parameter reference and update parameter naming

--- terraform/modules/application/parameters.tf
+++ terraform/modules/application/parameters.tf
@@ -1,10 +1,10 @@
 data "aws_ssm_parameter" "partial_database_connection_string" {
-  name = var.database_partial_connection_string_arn
+  name = var.database_partial_connection_string_parameter_name
 }
 
 resource "aws_ssm_parameter" "complete_database_connection_string" {
   #checkov:skip=CKV_AWS_337:default encryption not using a kms cmk sufficient
-  name  = "${var.prefix}_COMPLETE_DATABASE_CONNECTION_STRING"
+  name  = "${upper(var.prefix)}_COMPLETE_DATABASE_CONNECTION_STRING"
   type  = "SecureString"
   value = "${data.aws_ssm_parameter.partial_database_connection_string.value}/${var.database_name}"
 }
