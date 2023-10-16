dabf96252e7fc64a663f2c6f336e29f0699ac9e5
Sam Seed <sam.seed@softwire.com>
2023-10-16 16:35:28 +0100

    CLDC-2917: try giving root user all kms permissions

--- terraform/modules/bulk_upload/kms.tf
+++ terraform/modules/bulk_upload/kms.tf
@@ -24,4 +24,19 @@ data "aws_iam_policy_document" "kms" {
       aws_s3_bucket.bulk_upload.arn
     ]
   }
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions = [
+      "kms:*"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
 }
