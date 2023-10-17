31b40b9dd94518cc06fb946f0e637b8525cb23fd
Sam Seed <sam.seed@softwire.com>
2023-10-17 16:45:27 +0100

    CLDC-2918: create CMK for rds ssm parameters

---
+++ terraform/modules/rds/kms.tf
@@ -0,0 +1,52 @@
+resource "aws_kms_key" "this" {
+  description         = "KMS key used to encrypt SSM parameters."
+  enable_key_rotation = true
+}
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/${var.prefix}-rds-ssm-parameters"
+  target_key_id = aws_kms_key.this.key_id
+}
+
+resource "aws_kms_key_policy" "this" {
+  key_id = aws_kms_key.this.id
+  policy = data.aws_iam_policy_document.kms.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "kms" {
+  #checkov:skip=CKV_AWS_109:Only assigning the kms:GenerateDataKey and kms:Decrypt permissions led to a 'you won't be able to manage the key once created' error
+  #checkov:skip=CKV_AWS_111:Only assigning the kms:GenerateDataKey and kms:Decrypt permissions led to a 'you won't be able to manage the key once created' error
+  #checkov:skip=CKV_AWS_356:Only assigning the kms:GenerateDataKey and kms:Decrypt permissions led to a 'you won't be able to manage the key once created' error
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [var.ecs_task_execution_role_arn]
+    }
+
+    actions = [
+      "kms:GenerateDataKey",
+      "kms:Decrypt"
+    ]
+
+    resources = [
+      aws_kms_key.this.arn
+    ]
+  }
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions = [
+      "kms:*"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
--- terraform/modules/rds/parameters.tf
+++ terraform/modules/rds/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "database_partial_connection_string" {
-  #checkov:skip=CKV_AWS_337:default encryption not using a kms cmk sufficient
+  key_id = aws_kms_key.this.arn
   name  = "DATABASE_PARTIAL_CONNECTION_STRING"
   type  = "SecureString"
   value = "postgresql://${aws_db_instance.this.username}:${aws_db_instance.this.password}@${aws_db_instance.this.endpoint}"
