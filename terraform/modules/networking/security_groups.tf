7c4de66279a769cea28e49503a9697d1f854f851
Sam Seed <sam.seed@softwire.com>
2023-10-24 16:49:44 +0100

    CLDC-2907: use snake_case for tf resource names

--- terraform/modules/networking/security_groups.tf
+++ terraform/modules/networking/security_groups.tf
@@ -2,24 +2,24 @@ resource "aws_default_security_group" "main" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_default_security_group" "default-eu-west-2" {
-  vpc_id = data.aws_vpc.default-eu-west-2.id
+resource "aws_default_security_group" "default_eu_west_2" {
+  vpc_id = data.aws_vpc.default_eu_west_2.id
 }
 
-resource "aws_default_security_group" "default-eu-west-1" {
+resource "aws_default_security_group" "default_eu_west_1" {
   provider = aws.eu-west-1
 
-  vpc_id = data.aws_vpc.default-eu-west-1.id
+  vpc_id = data.aws_vpc.default_eu_west_1.id
 }
 
-resource "aws_default_security_group" "default-eu-west-3" {
+resource "aws_default_security_group" "default_eu_west_3" {
   provider = aws.eu-west-3
 
-  vpc_id = data.aws_vpc.default-eu-west-3.id
+  vpc_id = data.aws_vpc.default_eu_west_3.id
 }
 
-resource "aws_default_security_group" "default-us-east-1" {
+resource "aws_default_security_group" "default_us_east_1" {
   provider = aws.us-east-1
 
-  vpc_id = data.aws_vpc.default-us-east-1.id
+  vpc_id = data.aws_vpc.default_us_east_1.id
 }
