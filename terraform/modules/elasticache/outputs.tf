af73598c99ac1fbba29a58c99709736f64a42b9b
Chirag-Bhatti <Chirag.Bhatti@softwire.com>
2023-10-24 15:52:51 +0100

    CLDC-2597: update redis implementation for high and non-high availability to always have encryption at rest

--- terraform/modules/elasticache/outputs.tf
+++ terraform/modules/elasticache/outputs.tf
@@ -1,9 +1,5 @@
 output "redis_connection_string" {
-  value = var.highly_available ? (
-    "rediss://${aws_elasticache_replication_group.this[0].primary_endpoint_address}:${var.redis_port}"
-    ) : (
-    "rediss://${aws_elasticache_cluster.this[0].cache_nodes[0].address}:${aws_elasticache_cluster.this[0].cache_nodes[0].port}"
-  )
+  value = "rediss://${aws_elasticache_replication_group.this.primary_endpoint_address}:${var.redis_port}"
   description = "A connection string for connecting to the primary redis node, intended to be set in the environment variable REDIS_CONFIG"
   sensitive   = true
 }
--- terraform/modules/elasticache/redis.tf
+++ terraform/modules/elasticache/redis.tf
@@ -1,9 +1,6 @@
 resource "aws_elasticache_replication_group" "this" {
   #checkov:skip=CKV_AWS_31:TODO CLDC-2937 potentially introduce an auth token later
   #checkov:skip=CKV_AWS_191:default encryption key is sufficient
-
-  count = var.highly_available ? 1 : 0
-
   apply_immediately           = var.apply_changes_immediately
   at_rest_encryption_enabled  = true
   auto_minor_version_upgrade  = true
@@ -14,7 +11,7 @@ resource "aws_elasticache_replication_group" "this" {
   maintenance_window          = "sun:23:00-mon:01:30"
   multi_az_enabled            = true
   node_type                   = var.node_type
-  num_cache_clusters          = 2
+  num_cache_clusters          = var.highly_available ? 2 : 1
   parameter_group_name        = aws_elasticache_parameter_group.this.id
   port                        = var.redis_port
   preferred_cache_cluster_azs = ["eu-west-2a", "eu-west-2b"] # The first AZ in the list is where the primary node will be created. Replicas will be created in the following AZs.
@@ -24,25 +21,4 @@ resource "aws_elasticache_replication_group" "this" {
   snapshot_window             = "02:30-03:30"
   subnet_group_name           = var.redis_subnet_group_name
   transit_encryption_enabled  = true
-}
-
-resource "aws_elasticache_cluster" "this" {
-
-  count = var.highly_available ? 0 : 1
-
-  cluster_id                 = var.prefix
-  apply_immediately          = var.apply_changes_immediately
-  auto_minor_version_upgrade = true
-  engine                     = "redis"
-  engine_version             = "6.2"
-  maintenance_window         = "sun:23:00-mon:01:30"
-  node_type                  = var.node_type
-  num_cache_nodes            = 1
-  parameter_group_name       = aws_elasticache_parameter_group.this.id
-  port                       = var.redis_port
-  security_group_ids         = [var.redis_security_group_id]
-  snapshot_retention_limit   = var.snapshot_retention_limit
-  snapshot_window            = "02:30-03:30"
-  subnet_group_name          = var.redis_subnet_group_name
-  transit_encryption_enabled = true
 }
