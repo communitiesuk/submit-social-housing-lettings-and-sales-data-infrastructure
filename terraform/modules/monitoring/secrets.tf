d8a8e8fbffed8befa4cba5eaa87331200a64e753
Sam Seed <sam.seed@softwire.com>
2023-10-31 11:48:26 +0000

    CLDC=2921: first attempt at defining secure email subscription

---
+++ terraform/modules/monitoring/secrets.tf
@@ -0,0 +1,5 @@
+resource "aws_secretsmanager_secret" "email" {
+  #checkov:skip=CKV2_AWS_57:secret doesn't require automatic rotation
+  name       = "MONITORING_EMAIL"
+  kms_key_id = aws_kms_key.this.arn
+}
