a0fa79ea95e6d005b49890a33c228867568e3ad8
Sam Seed <sam.seed@softwire.com>
2023-10-31 12:52:55 +0000

    CLDC-2921: finalise KMS setup

--- terraform/modules/monitoring/secrets.tf
+++ terraform/modules/monitoring/secrets.tf
@@ -1,7 +1,7 @@
 resource "aws_secretsmanager_secret" "email" {
   #checkov:skip=CKV2_AWS_57:secret doesn't require automatic rotation
   name       = "MONITORING_EMAIL"
-  # kms_key_id = aws_kms_key.this.arn
+  kms_key_id = aws_kms_key.this.arn
 }
 
 data "aws_secretsmanager_secret_version" "email" {
