402a1fbcd6c3f20ee4f6efca43c9b08874278a7a
Sam Seed <sam.seed@softwire.com>
2023-10-31 12:37:16 +0000

    CLDC-2921: get secret value from secret version data source

--- terraform/modules/monitoring/subscriptions.tf
+++ terraform/modules/monitoring/subscriptions.tf
@@ -1,5 +1,5 @@
 resource "aws_sns_topic_subscription" "email" {
   topic_arn = aws_sns_topic.this.arn
   protocol  = "email"
-  endpoint  = aws_secretsmanager_secret.email.arn
+  endpoint  = data.aws_secretsmanager_secret_version.email.secret_string
 }
