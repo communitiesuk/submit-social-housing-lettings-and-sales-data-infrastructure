a4151a36ffc9d9256162d40d1979c16d7c4dbaff
Rachael Booth <rachael.booth@softwire.com>
2023-11-07 15:10:02 +0000

    Allow infra repo role access to lock table

--- terraform/modules/github_actions_access/infra_repo_policies.tf
+++ terraform/modules/github_actions_access/infra_repo_policies.tf
@@ -1,10 +1,10 @@
 # TODO CLDC-2820: may be able to restrict this to specific files
-data "aws_iam_policy_document" "state_bucket_access" {
+data "aws_iam_policy_document" "state_access" {
   statement {
     actions = [
       "s3:ListBucket"
     ]
-    resources = var.state_bucket_arns
+    resources = var.state_details[*].bucket_arn
     effect    = "Allow"
   }
 
@@ -14,17 +14,28 @@ data "aws_iam_policy_document" "state_bucket_access" {
       "s3:PutObject",
       "s3:DeleteObject"
     ]
-    resources = formatlist("%s/*", var.state_bucket_arns)
+    resources = formatlist("%s/*", var.state_details[*].bucket_arn)
     effect    = "Allow"
+  }
+
+  statement {
+    actions = [
+"dynamodb:DescribeTable",
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem"
+    ]
+    resources = var.state_details[*].lock_table_arn
+    effect = "Allow"
   }
 }
 
-resource "aws_iam_policy" "state_bucket_access" {
+resource "aws_iam_policy" "state_access" {
   name   = "state-bucket-access"
-  policy = data.aws_iam_policy_document.state_bucket_access.json
+  policy = data.aws_iam_policy_document.state_access.json
 }
 
-resource "aws_iam_role_policy_attachment" "infra_repo_state_bucket_access" {
+resource "aws_iam_role_policy_attachment" "infra_repo_state_access" {
   role       = aws_iam_role.repo["infrastructure"].name
-  policy_arn = aws_iam_policy.state_bucket_access.arn
+  policy_arn = aws_iam_policy.state_access.arn
 }
