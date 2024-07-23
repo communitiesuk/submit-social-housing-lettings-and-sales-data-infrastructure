4c26357d2ec601f4c9a1e1d32343ddb5e6c4dabf
Rachael Booth <rachael.booth@softwire.com>
2024-07-23 17:27:20 +0100

    Use customer managed key for scheduler

--- terraform/modules/rds/scheduled_stop.tf
+++ terraform/modules/rds/scheduled_stop.tf
@@ -6,6 +6,8 @@ resource "aws_scheduler_schedule" "rds_start" {
   schedule_expression          = "cron(${var.scheduled_stop.timings.workday_start} ? * MON-FRI *)"
   schedule_expression_timezone = "Europe/London"
 
+  kms_key_arn = aws_kms_key.scheduler[0].arn
+
   flexible_time_window {
     mode = "OFF"
   }
@@ -26,6 +28,8 @@ resource "aws_scheduler_schedule" "rds_stop" {
 
   schedule_expression          = "cron(${var.scheduled_stop.timings.workday_end} ? * MON-FRI *)"
   schedule_expression_timezone = "Europe/London"
+
+  kms_key_arn = aws_kms_key.scheduler[0].arn
 
   flexible_time_window {
     mode = "OFF"
@@ -88,4 +92,40 @@ resource "aws_iam_role_policy_attachment" "scheduler_allow_rds_actions" {
 
   role       = aws_iam_role.scheduler[0].name
   policy_arn = aws_iam_policy.allow_rds_actions[0].arn
+}
+
+resource "aws_kms_key" "scheduler" {
+  count = var.scheduled_stop.enabled ? 1 : 0
+
+  description         = "KMS key used for rds scheduling"
+  enable_key_rotation = true
+}
+
+resource "aws_kms_alias" "scheduler" {
+  count = var.scheduled_stop.enabled ? 1 : 0
+
+  name          = "alias/${var.prefix}-rds-scheduler"
+  target_key_id = aws_kms_key.scheduler[0].key_id
+}
+
+resource "aws_kms_key_policy" "scheduler" {
+  count = var.scheduled_stop.enabled ? 1 : 0
+
+  key_id = aws_kms_key.scheduler[0].id
+  policy = data.aws_iam_policy_document.scheduler[0].json
+}
+
+data "aws_iam_policy_document" "scheduler" {
+  count = var.scheduled_stop.enabled ? 1 : 0
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions = ["kms:*"]
+
+    resources = [aws_kms_key.scheduler[0].arn]
+  }
 }
