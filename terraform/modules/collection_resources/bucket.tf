60e02e4edd6f1b04788e919d88ba5b15c5420237
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-09-30 12:36:39 +0100

    Make bucket public read

--- terraform/modules/collection_resources/bucket.tf
+++ terraform/modules/collection_resources/bucket.tf
@@ -15,10 +15,27 @@ resource "aws_s3_bucket_logging" "access_logging" {
 resource "aws_s3_bucket_public_access_block" "collection_resources" {
   bucket = aws_s3_bucket.collection_resources.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.collection_resources.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.collection_resources.arn}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_policy" "force_ssl" {
