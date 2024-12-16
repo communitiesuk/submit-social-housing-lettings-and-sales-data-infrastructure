bcf5b12d4abf501f944c4d7d4307a8acdaf3961f
Rachael Booth <rachael.booth@softwire.com>
2024-12-16 17:47:44 +0000

    CLDC-3810: Set up ability to automatically redeploy app when collection year rolls over

---
+++ terraform/modules/application/scheduled_redeploy.tf
@@ -0,0 +1,71 @@
+resource "aws_scheduler_schedule" "collection_rollover_redeploy" {
+  count = var.collection_rollover_redeploy_enabled ? 1 : 0
+
+  name = "${var.prefix}-app-collection-rollover-redeploy"
+
+  schedule_expression          = "cron(40 17 16 12 ? *)"
+  schedule_expression_timezone = "UTC"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  target {
+    arn      = "arn:aws:scheduler:::aws-sdk:ecs:updateService"
+    role_arn = aws_iam_role.scheduler[0].arn
+
+    input = jsonencode({
+      Cluster            = aws_ecs_cluster.this.name,
+      Service            = aws_ecs_service.app.name,
+      ForceNewDeployment = true
+    })
+  }
+}
+
+resource "aws_iam_role" "scheduler" {
+  count = var.collection_rollover_redeploy_enabled ? 1 : 0
+
+  name               = "${var.prefix}-rds-scheduler"
+  assume_role_policy = data.aws_iam_policy_document.scheduler_assume_role[0].json
+}
+
+data "aws_iam_policy_document" "scheduler_assume_role" {
+  count = var.collection_rollover_redeploy_enabled ? 1 : 0
+
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["scheduler.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "allow_ecs_actions" {
+  count = var.collection_rollover_redeploy_enabled ? 1 : 0
+
+  statement {
+    actions = [
+      "ecs:Describe*",
+      "ecs:UpdateService"
+    ]
+    resources = [aws_ecs_service.app.id]
+    effect    = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "allow_ecs_actions" {
+  count = var.collection_rollover_redeploy_enabled ? 1 : 0
+
+  name   = "${var.prefix}-allow-ecs-actions"
+  policy = data.aws_iam_policy_document.allow_ecs_actions[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "scheduler_allow_ecs_actions" {
+  count = var.collection_rollover_redeploy_enabled ? 1 : 0
+
+  role       = aws_iam_role.scheduler[0].name
+  policy_arn = aws_iam_policy.allow_ecs_actions[0].arn
+}
