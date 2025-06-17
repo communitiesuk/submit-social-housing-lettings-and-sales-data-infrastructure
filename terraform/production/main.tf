ad8e228ca7f0a17673dda84064bdd57c4b2b2cc6
Samuel <samuel.young@softwire.com>
2025-06-17 16:13:56 +0000

    terraform format

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -96,7 +96,7 @@ module "application" {
   sidekiq_task_desired_count = 2
   sidekiq_task_memory        = 8192
 
-  ad_hoc_task_cpu = 1024
+  ad_hoc_task_cpu    = 1024
   ad_hoc_task_memory = 8192
 
   out_of_hours_scale_down = {
