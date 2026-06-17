07bb14c0cfb26da33cfc4b81f32e82e777d6391a
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-06-17 15:12:27 +0100

    feat: don't specify AZ when not highly_available

--- terraform/modules/elasticache/redis.tf
+++ terraform/modules/elasticache/redis.tf
@@ -1,5 +1,5 @@
 locals {
-  preferred_cache_cluster_azs = var.highly_available ? ["eu-west-2a", "eu-west-2b"] : ["eu-west-2a"]
+  preferred_cache_cluster_azs = var.highly_available ? ["eu-west-2a", "eu-west-2b"] : null
 }
 
 resource "aws_elasticache_replication_group" "this" {
